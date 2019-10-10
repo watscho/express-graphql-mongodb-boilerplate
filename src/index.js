ef1e782efdff0db0b03597d72601e763abe159ba
watscho <w4tscho@gmail.com>
2019-10-10 12:24:48 +0400

    fix: rename i18next middleware "middleware - i18nextMiddleware"

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { apolloUploadExpress } = require('apollo-upload-server')
 const i18next = require('i18next')
-const middleware = require('i18next-express-middleware')
+const i18nextMiddleware = require('i18next-express-middleware')
 require('dotenv').config()
 require('module-alias/register')
 
+require('@app/redis')
 require('@app/i18next')
 
 const authentication = require('@app/middleware/authentication')
 
-require('@app/redis')
 const graphql = require('@app/graphql')
 
 const app = express()
 
-app.use(middleware.handle(i18next))
+app.use(i18nextMiddleware.handle(i18next))
 
 app.use(
   '/graphql',
