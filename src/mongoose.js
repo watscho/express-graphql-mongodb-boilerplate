bb17e8936cb34991f8fdee23b592b6f942814896
watscho <w4tscho@gmail.com>
2019-10-15 12:45:13 +0400

    fix: export db port to .env

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-  .connect(`${process.env.DB_HOST}/${process.env.DB_NAME}`, {
+  .connect(`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
