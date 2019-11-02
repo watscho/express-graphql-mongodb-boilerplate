c441ae71bf5e98fb426e225916370bed19a70b58
watscho <w4tscho@gmail.com>
2019-11-02 17:27:47 +0400

    fix: clear unused code

--- src/module/auth/service/index.js
+++ src/module/auth/service/index.js
@@ -1,7 +1,5 @@
 const UserService = require('@app/module/auth/service/userService').getInstance()
 
 module.exports = {
-  verifyRequestService: UserService.verifyRequest,
-  clearExpiredVerificationsService: UserService.clearExpiredVerifications,
-  clearExpiredResetPasswordsService: UserService.clearExpiredResetPasswords
+  verifyRequestService: UserService.verifyRequest
 }
