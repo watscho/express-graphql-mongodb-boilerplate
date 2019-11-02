20ce6b9aabb6fd742ceefc06703fe21c757581a6
watscho <w4tscho@gmail.com>
2019-11-02 15:27:33 +0400

    refactor: rm worker from api

--- worker/jobs/index.js
+++
@@ -1,4 +0,0 @@
-const clearExpiredResetPasswords = require('@cron/jobs/clearExpiredResetPasswords')
-const clearExpiredVerifications = require('@cron/jobs/clearExpiredVerifications')
-
-module.exports = { clearExpiredResetPasswords, clearExpiredVerifications }
