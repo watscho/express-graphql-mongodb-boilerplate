851b478ef8ae1baefcdb66fcef7506f38fb06dff
watscho <w4tscho@gmail.com>
2019-11-05 12:22:30 +0400

    refactor: data insert

--- src/module/auth/service/userService.js
+++ src/module/auth/service/userService.js
@@ -2,21 +2,25 @@ const crypto = require('crypto-random-string')
 const moment = require('moment')
 
 class UserService {
-  async verifyRequest (user) {
+  async verifyRequest(user) {
     const token = crypto({ length: 48, type: 'url-safe' })
     const expireIn = moment().add(7, 'days')
 
-    user.account.verification = {
-      token: token,
-      expireIn: expireIn
-    }
+    user.set({
+      account: {
+        verification: {
+          token,
+          expireIn
+        }
+      }
+    })
 
     await user.save()
 
     return token
   }
 
-  static getInstance () {
+  static getInstance() {
     return new this()
   }
 }
