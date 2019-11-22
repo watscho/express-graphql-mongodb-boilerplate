3362f4ebf68bcfcabf54c50fdfc7445f7160fee5
watscho <w4tscho@gmail.com>
2019-11-22 15:49:50 +0400

    fix: rename middleware to i18nextMiddleware for i18next

--- src/i18next/index.js
+++ src/i18next/index.js
@@ -1,10 +1,10 @@
 const i18next = require('i18next')
-const middleware = require('i18next-express-middleware')
+const i18nextMiddleware = require('i18next-express-middleware')
 
 const localeEN = require('@app/i18next/locales/en.json')
 const localeGE = require('@app/i18next/locales/ge.json')
 
-i18next.use(middleware.LanguageDetector).init({
+i18next.use(i18nextMiddleware.LanguageDetector).init({
   detection: {
     order: ['header'],
     lookupHeader: 'accept-language'
