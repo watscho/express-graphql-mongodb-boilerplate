d62ab5bceaac348e57cd88a636c7afbf35650e0f
watscho <w4tscho@gmail.com>
2019-11-22 16:21:12 +0400

    feat: log for mongoose connection

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose')
+const winston = require('winston')
 
 mongoose
   .connect(`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .catch(err => {
-    // eslint-disable-next-line no-console
-    console.log(err)
+  .catch(error => {
+    winston.error(error)
   })
 
 mongoose.connection.on('open', () => {
-  // eslint-disable-next-line no-console
-  console.log('MongoDB connected')
+  winston.info('MongoDB connected')
 })
 
 module.exports = mongoose
