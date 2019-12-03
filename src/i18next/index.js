9874026c62fa132f350e19821ac4be62e16d3cca
watscho <w4tscho@gmail.com>
2019-12-03 15:24:18 +0400

    fix: language gql type from i18next config - rollback

--- src/i18next/index.js
+++ src/i18next/index.js
@@ -4,26 +4,16 @@ const i18nextMiddleware = require('i18next-express-middleware')
 const localeEN = require('@app/i18next/locales/en.json')
 const localeGE = require('@app/i18next/locales/ge.json')
 
-const locales = ['en', 'ge']
-
 i18next.use(i18nextMiddleware.LanguageDetector).init({
   detection: {
     order: ['header'],
     lookupHeader: 'accept-language'
   },
-  preload: locales,
-  whitelist: locales,
+  preload: ['en', 'ge'],
+  whitelist: ['en', 'ge'],
   fallbackLng: 'en',
   resources: {
     en: { translation: localeEN },
     ge: { translation: localeGE }
   }
 })
-
-const graphQLEnumTC = {}
-
-for (let i = 0; i < locales.length; i++) {
-  graphQLEnumTC[locales[i]] = { value: locales[i] }
-}
-
-i18next.graphQLEnumTC = graphQLEnumTC
