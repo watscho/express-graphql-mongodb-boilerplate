3278870c993ffb8c0bc4edaf12bd42a0d8b344f2
watscho <w4tscho@gmail.com>
2019-12-05 02:19:00 +0400

    fix: redis connect k-order

--- src/redis.js
+++ src/redis.js
@@ -2,8 +2,8 @@ const Redis = require('ioredis')
 const winston = require('winston')
 
 const client = new Redis({
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOST
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
 })
 
 let wasError = false
