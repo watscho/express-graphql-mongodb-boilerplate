7e0f398d6ee55034c86c5a0f43b11a8f2cbe3e4c
watscho <w4tscho@gmail.com>
2019-12-06 21:55:24 +0400

    fix: rm selfish-proxy

--- src/validator/userValidator.js
+++ src/validator/userValidator.js
@@ -1,22 +1,19 @@
 const validator = require('validator')
-const { selfish } = require('selfish-proxy')
+
+const normalizeEmailOptions = {
+  all_lowercase: true,
+  gmail_lowercase: true,
+  gmail_remove_dots: true,
+  gmail_remove_subaddress: true,
+  gmail_convert_googlemaildotcom: true,
+  outlookdotcom_remove_subaddress: true,
+  yahoo_lowercase: true,
+  yahoo_remove_subaddress: true,
+  icloud_lowercase: true,
+  icloud_remove_subaddress: true
+}
 
 class UserValidator {
-  constructor () {
-    this.normalizeEmailOptions = {
-      all_lowercase: true,
-      gmail_lowercase: true,
-      gmail_remove_dots: true,
-      gmail_remove_subaddress: true,
-      gmail_convert_googlemaildotcom: true,
-      outlookdotcom_remove_subaddress: true,
-      yahoo_lowercase: true,
-      yahoo_remove_subaddress: true,
-      icloud_lowercase: true,
-      icloud_remove_subaddress: true
-    }
-  }
-
   async changePassword (resolve, source, args, context, info) {
     const { newPassword } = args
 
@@ -39,21 +36,21 @@ class UserValidator {
 
   async resetPassword (resolve, source, args, context, info) {
     args.email = validator.trim(args.email)
-    args.email = validator.normalizeEmail(args.email, this.normalizeEmailOptions)
+    args.email = validator.normalizeEmail(args.email, normalizeEmailOptions)
 
     return resolve(source, args, context, info)
   }
 
   async signIn (resolve, source, args, context, info) {
     args.email = validator.trim(args.email)
-    args.email = validator.normalizeEmail(args.email, this.normalizeEmailOptions)
+    args.email = validator.normalizeEmail(args.email, normalizeEmailOptions)
 
     return resolve(source, args, context, info)
   }
 
   async signUp (resolve, source, args, context, info) {
     args.email = validator.trim(args.email)
-    args.email = validator.normalizeEmail(args.email, this.normalizeEmailOptions)
+    args.email = validator.normalizeEmail(args.email, normalizeEmailOptions)
 
     const { email, password } = args
 
@@ -70,7 +67,7 @@ class UserValidator {
 
   async updateUser (resolve, source, args, context, info) {
     args.email = validator.trim(args.email)
-    args.email = validator.normalizeEmail(args.email, this.normalizeEmailOptions)
+    args.email = validator.normalizeEmail(args.email, normalizeEmailOptions)
 
     const { email, firstName, lastName } = args
 
@@ -89,7 +86,7 @@ class UserValidator {
 
   static getInstance () {
     if (!this.instance) {
-      this.instance = selfish(new this())
+      this.instance = new this()
     }
     return this.instance
   }
